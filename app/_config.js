66105c6296355aa0d4f55e55f47e58f5e2dcf7c8
wommy <tom@technomad.media>
2019-08-19 12:51:39 -0400

    olde, no clue

--- app/_config.js
+++ app/_config.js
@@ -1,4 +1,4 @@
-beware pseudo code
+=== beware pseudo code
 
 look in _pages
 	apply _template to every other file
@@ -9,3 +9,31 @@ look in _pages
 
 apply [ var 1,2,3 ] to _template
 	prepend _template to about
+
+===
+
+applying varible to _template is too much work
+TODO
+
+===
+
+look in _pages
+look for _template.html
+stringify that
+	var _template = `
+	<html>
+	</html>
+		<head>
+		</head>
+		<body id='root'>
+			<main id='content'>
+				`#{content}`
+			</main>
+		</body>
+
+	`
+break into two parts @content
+for every page in _pages/
+
+	
+
